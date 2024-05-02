32af017251fd6cc98382f5295e398f3680e7fd79
cassanof <federico.cassano@federico.codes>
2024-05-01 19:35:09 -0700

    able to set custom engine

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker build -t python-code-exec .
-docker container rm -f python-code-exec 2>/dev/null
+V_ENGINE=${ENGINE:-docker}
+$V_ENGINE build -t python-code-exec .
+$V_ENGINE container rm -f python-code-exec 2>/dev/null
 sleep 1
-docker run --name python-code-exec --restart always -d -p 8000:8000 python-code-exec
+$V_ENGINE run --name python-code-exec --restart always -d -p 8000:8000 python-code-exec
