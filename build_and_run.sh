b44d4742fbf9b9b0cd858455a4d06b72c843e4d6
cassanof <federico.cassano@scale.com>
2024-05-09 17:24:57 -0700

    increase file limi

--- build_and_run.sh
+++ build_and_run.sh
@@ -4,4 +4,4 @@ V_ENGINE=${ENGINE:-docker}
 $V_ENGINE build -t code-exec .
 $V_ENGINE container rm -f code-exec 2>/dev/null
 sleep 1
-$V_ENGINE run --name code-exec --restart always -d -p 8000:8000 code-exec
+$V_ENGINE run --name code-exec --restart always -d --ulimit nofile=1000000:1000000 -p 8000:8000 code-exec
